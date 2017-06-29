95b2d0b2b9baba88c80a89290c5ba89d5475dfd0
Gabriele Vassallo <gabri.vassallo01@gmail.com>
2017-06-29 16:52:56 +0200

    Update

--- res/gulpfile.js
+++ res/gulpfile.js
@@ -3,15 +3,15 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat'); 
 
 gulp.task('min', function(){
-     return gulp.src(['runtime.js', 'classie.js', 'nav.js'])
-                .pipe(uglify())
-                .pipe(concat('scripts.js'))
-                .pipe(gulp.dest('./'))
+  return gulp.src(['runtime.js', 'classie.js', 'nav.js'])
+  .pipe(uglify())
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('./'))
 });
 
 gulp.task('debug', function(){
-     return gulp.src(['runtime.js', 'classie.js', 'nav.js'])
-                .pipe(concat('scripts.js'))
-                .pipe(gulp.dest('./'))
+  return gulp.src(['runtime.js', 'classie.js', 'nav.js'])
+  .pipe(concat('scripts.js'))
+  .pipe(gulp.dest('./'))
 });
 gulp.task('default', ['min']);
